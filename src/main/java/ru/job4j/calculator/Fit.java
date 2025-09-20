33e5bbdf3609be5a88c4f9f043d4088c9d811074
marat92 <sodnopov@list.ru>
2025-09-20 20:54:50 +0700

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,21 +1,19 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short height) {
-        double result = (height - 100) * 1.15;
-        return result;
+    public static double manWeight(int height) {
+        return (height - 100) * 1.15;
     }
 
-    public static double womanWeight(short height) {
-        double result = (height - 110) * 1.15;
-        return result;
+    public static double womanWeight(int height) {
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
-        short height = 180;
+        int height = 180;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 is " + man);
-        short heightWoman = 170;
+        int heightWoman = 170;
         double woman = Fit.womanWeight(heightWoman);
         System.out.println("Woman 167 is " + woman);
     }
--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -8,7 +8,7 @@ class FitTest {
 
     @Test
     void whenMan180Then92() {
-        short input = 180;
+        int input = 180;
         double expected = 92;
         double output = Fit.manWeight(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
@@ -16,7 +16,7 @@ class FitTest {
 
     @Test
     void whenWoman170Then69() {
-        short input = 170;
+        int input = 170;
         double expected = 69;
         double output = Fit.womanWeight(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
