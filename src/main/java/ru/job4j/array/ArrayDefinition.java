dbcfeeac824b7b2c58e726fc157a3b0df54a30f6
marat92 <sodnopov@list.ru>
2025-09-21 20:47:03 +0700

    6.0.1. Объявление массива. [#3613 #34831]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+/*
+- Массив на 10 элементов типа short с именем ages.
+    - Массив на 100500 элементов типа String c именем surnames.
+    - Массив на 40 элементов типа float с именем prices.
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
